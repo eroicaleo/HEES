c5fd602980cc07c2ee159604561f41578c46a166
Yang Ge <yangge@broadcom.com>
2014-02-26 11:22:20 -0800

    Added ScheduleBuilder class
      This class uses neural network predictor
      to predict the energy for a given schedule

---
+++ ScheduleBuilder.cpp
@@ -0,0 +1,68 @@
+#include <fstream>
+#include <tr1/functional>
+
+#include "nnet/nnetmultitask.hpp"
+#include "powersource.hpp"
+#include "ScheduleBuilder.hpp"
+
+using namespace std;
+using namespace std::tr1;
+
+extern const double CRAZY_ENERGY;
+extern ConstantPowerSource cps;
+
+void ScheduleBuilder::BuildScheduleFromFile(const char *filename) {
+	double vol, cur;
+	int lastFinish(0), len(0);
+	int realTaskID(0), idleTaskID(0); 
+	dpTableEntry entry;
+
+	ifstream infile(filename);
+	while ((infile >> vol >> cur >> len).good()) {
+		if (cur > 0.0) {
+			entry.setAllFields(CRAZY_ENERGY, vol, cur, -1, realTaskID, len, lastFinish);
+			++realTaskID;
+		} else {
+			entry.setAllFields(CRAZY_ENERGY, vol, cur, -1, idleTaskID, len, lastFinish);
+			++idleTaskID;
+		}
+		lastFinish += len;
+		m_schedule.push_back(entry);
+	}
+	infile.close();
+}
+
+void ScheduleBuilder::PredictEnergyForSchedule(double initEnergy) {
+
+	dcconvertIN dcload;
+	double inputPower(0.0);
+	double currentEnergy(initEnergy);
+	nnetmultitask nnetPredictor;
+	std::tr1::function<double(double, double, double)> energyCalculator;
+	energyCalculator = bind(&nnetmultitask::predictWithEnergyLength, nnetPredictor, placeholders::_1, placeholders::_2, placeholders::_3);
+
+	for (tableEntryIter iter = m_schedule.begin(); iter != m_schedule.end(); ++iter) {
+		inputPower = cps.GetConstantPower(0.0) - dcload.GetPowerConsumptionWithLoad(iter->voltage, iter->current);
+		currentEnergy = energyCalculator(inputPower, currentEnergy, iter->len);
+		iter->totalEnergy = currentEnergy;
+	}
+
+	return;
+}
+
+void ScheduleBuilder::DumpSchedule(ostream &os) const {
+
+	tableEntryConstIter iter;
+	os << endl;
+	os << "#########################################################" << endl;
+	os << "############### Begin to dump schedule! #################" << endl;
+	os << "#########################################################" << endl;
+	for (iter = m_schedule.begin(); iter != m_schedule.end(); ++iter) {
+		os << *iter << endl;
+	}
+	os << "#########################################################" << endl;
+	os << "###############  End to dump schedule!  #################" << endl;
+	os << "#########################################################" << endl;
+	os << endl;
+
+}
---
+++ ScheduleBuilder.hpp
@@ -0,0 +1,22 @@
+#ifndef _SCHEDULE_BUILDER_HPP_
+#define _SCHEDULE_BUILDER_HPP_
+
+#include <iostream>
+#include <vector>
+
+#include "DP/taskScheduling.hpp"
+
+using std::vector;
+using std::ostream;
+
+class ScheduleBuilder {
+public:
+	// Leave all constructor/destructor, copy/move operations default
+	void BuildScheduleFromFile(const char *filename);
+	void PredictEnergyForSchedule(double initEnergy);
+	void DumpSchedule(ostream &os = std::cout) const;
+private:
+	vector<dpTableEntry> m_schedule;
+};
+
+#endif
