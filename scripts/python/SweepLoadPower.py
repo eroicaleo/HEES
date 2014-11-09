e01ed3f006a034a6e7744445e52d344ee78a3bc1
Yukan Zhang <yzhan158@syr.edu>
2014-11-08 21:05:18 -0800

    Added scripts to sweep load power

---
+++ scripts/python/SweepLoadPower.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+'''
+This script sweeps the load power and
+generate supercapacitor energy curve.
+
+1. Run it in the directory with the ./energysys
+2. The output is in the SweepLoad.txt
+3. Use matlab/octave PlotSweepLoad.m to plot the data
+'''
+
+import os
+import os.path
+import re
+import subprocess
+import sys
+
+def processLogFile():
+  f = open('OverallProcess.txt', 'r')
+  # Skip the header
+  f.readline()
+  # energy = [[re.split(r'\s+', line) for line in f][i] for i in [7, 10]]
+  res = [[re.split(r'\s+', line)[i] for i in [7, 10]] for line in f]
+  energy = [i[1] for i in res]
+  inputPower = res[0][0]
+  return inputPower, energy
+
+##----------------------------------------------------------------------------------------------------
+## Sanity check
+##----------------------------------------------------------------------------------------------------
+if not os.path.isfile('energysys'):
+  sys.exit('Can not find energysys binary in current dir!')
+
+##----------------------------------------------------------------------------------------------------
+## Generate load power sequence
+##----------------------------------------------------------------------------------------------------
+initalPower = 0.6
+step        = 0.2
+tasklen     = 10000
+NumofLoad   = 1
+LoadPower = [initalPower+step*x for x in range(NumofLoad)]
+
+energyList = []
+
+for power in LoadPower:
+  f = open('Tasks.txt', 'w')
+  print('1.0 %.2f %s' % (power, tasklen), file=f)
+  f.close()
+  print('Run the load power ', power)
+  # Run the simulator
+  f1 = open('1', 'w')
+  subprocess.call(['./energysys'], stdout=f1, stderr=f1)
+  f1.close()
+  # Collect the power number
+  inputPower, energy = processLogFile()
+  energyList.append([inputPower] + energy)
+  # Dump out the results
+  f = open('SweepLoad.txt', 'w')
+  for i in range(len(energyList[0])):
+    for j in range(len(energyList)):
+      print('%8.3f'%(float(energyList[j][i])), file=f, end=' '*4)
+    print(file=f)
+  f.close()
