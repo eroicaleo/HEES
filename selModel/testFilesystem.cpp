a046b5fcfba68a7409836ce304400a357f92edbc
Yukan Zhang <yzhan158@syr.edu>
2015-11-04 02:05:18 -0500

    updates selModel/testFilesystem.cpp
      Reading models from all model files

--- selModel/testFilesystem.cpp
+++ selModel/testFilesystem.cpp
@@ -1,39 +1,141 @@
 #include <algorithm>
 #include <iostream>
 #include <functional>
+#include <map>
+
 #include <boost/filesystem.hpp>
+#include <boost/xpressive/xpressive.hpp>
+
+#include "testFilesystem.hpp"
 
 using namespace boost::filesystem;
+using namespace boost::xpressive;
 using std::cout;
 using std::endl;
-using std::string;
-using std::vector;
-using std::transform;
+using std::make_pair;
+using std::map;
 using std::mem_fun_ref;
+using std::pair;
+using std::string;
+using std::transform;
+using std::vector;
 
-// http://stackoverflow.com/questions/1257721/can-i-use-a-mask-to-iterate-files-in-a-directory-with-boost
+/*
+ * General reference:
+ *   1. http://www.boost.org/doc/libs/1_59_0/libs/filesystem/doc/reference.html
+ *   2. http://www.boost.org/doc/libs/1_59_0/libs/filesystem/doc/tutorial.html
+ *   3. http://www.boost.org/doc/libs/1_49_0/doc/html/xpressive/user_s_guide.html
+ *   4. http://stackoverflow.com/questions/1257721/can-i-use-a-mask-to-iterate-files-in-a-directory-with-boost
+ */
 
 int main(int argc, char *argv[]) {
+
+	map<double, Level2Map> nnetMap;
+	buildNnetMap(nnetMap);
+
+	cout << "###############################" << endl;
+	cout << "## Start recall process" << endl;
+	cout << "###############################" << endl;
+
+	std::vector<pair<double, double> > testcase;
+	testcase.push_back(make_pair(1400.0, 0.5));
+	testcase.push_back(make_pair(1400.0, 1.5));
+	testcase.push_back(make_pair(1400.0, 2.5));
+	for (std::vector<pair<double, double> >::iterator iter(testcase.begin()); iter != testcase.end(); ++iter) {
+		const nnetmodel &nmodel = recallNnetMap(nnetMap, iter->first, iter->second);
+		cout << "Bank voltage: " << nmodel.bank_vol << endl;
+		cout << "Solar min: " << nmodel.solar_min << endl;
+		cout << "Solar max: " << nmodel.solar_max << endl;
+		cout << "Energy offset: " << nmodel.energy_offset << endl;
+		cout << nmodel.IW << endl;
+		cout << nmodel.LW << endl;
+	}
+	return 0;
+}
+
+const nnetmodel &recallNnetMap(const map<double, Level2Map> &nnetMap, double bankCharge, double solarPower) {
+
+	// Find the 1st levle key
+	if (nnetMap.upper_bound(bankCharge) == nnetMap.begin()) {
+		std::cerr << "bank charge: " << bankCharge << " is out of training range!"
+			<< " The min training range is: " << nnetMap.begin()->first << endl;
+		exit(-1);
+	}
+	const Level2Map &l2map = (--nnetMap.upper_bound(bankCharge))->second;
+
+	// Find the 2nd level key
+	if (l2map.upper_bound(solarPower) == l2map.begin()) {
+		std::cerr << "sun charge: " << solarPower << " is out of training range!"
+			<< " The min training rage is: " << l2map.begin()->first << endl;
+		exit(-1);
+	}
+	const nnetmodel &nmodel = (--l2map.upper_bound(solarPower))->second;
+
+	return nmodel;
+}
+
+void buildNnetMap(map<double, Level2Map> &nnetMap) {
 
 	// To avoid the run time error:
 	// terminate called after throwing an instance of 'std::runtime_error'
 	// what():  locale::facet::_S_create_c_locale name not valid
 	// https://svn.boost.org/trac/boost/ticket/5928
 
+	double bank_vol = 0.0;
+	double solar_min = 0.0;
+	double solar_max = 10000.0;
+
 	path model_dir(string("./data/PredictionModel/"));
 	if (is_directory(model_dir)) {
-		vector<directory_entry> v_entry;
-		vector<path> v;
+		std::vector<directory_entry> v_entry;
+		std::vector<path> v;
 
 		// http://stackoverflow.com/questions/458525/iterating-over-vector-and-calling-functions
 		// http://www.cplusplus.com/reference/functional/mem_fun_ref/
 		copy(directory_iterator(model_dir), directory_iterator(), back_inserter(v_entry));
 		transform(v_entry.begin(), v_entry.end(), back_inserter(v), mem_fun_ref(&directory_entry::path));
 
-		for (vector<path>::const_iterator it(v.begin()); it != v.end(); ++it) {
-			cout << *it << endl;
+		sregex rex1 = sregex::compile("nnetmodel(\\d+)");
+		sregex rex2 = sregex::compile("nnetmodel(\\d+)_([\\d.]+)_([\\d.]+|inf)");
+		smatch what;
+		for (std::vector<path>::const_iterator it(v.begin()); it != v.end(); ++it) {
+			cout << it->filename() << endl;
+
+			solar_min = 0.0;
+			solar_max = 10000.0;
+
+			// http://stackoverflow.com/questions/4179322/how-to-convert-boost-path-type-to-string
+			if (regex_match(it->filename().string(), what, rex1)) {
+
+			} else if (regex_match(it->filename().string(), what, rex2)) {
+				solar_min = atof(what.str(2).c_str());
+				if (what.str(3) != "inf") {
+					solar_max = atof(what.str(3).c_str());
+				}
+			} else {
+				std::cerr << "Skip wrong filename: " << it->filename() << '\n';
+				continue;
+			}
+			bank_vol = atof(what.str(1).c_str());
+			std::cout << "bank voltage: " << bank_vol << std::endl;
+			std::cout << "solar min: " << solar_min << std::endl;
+			std::cout << "solar max: " << solar_max << std::endl;
+
+			/* Read in the neural network model */
+			struct nnetmodel nnet;
+			nnet.readnnetmodel(it->string().c_str());
+			nnet.energy_offset = 0.5 * pow(bank_vol, 2) / 40.0; 
+			nnet.bank_vol = bank_vol;
+			nnet.solar_min = solar_min;
+			nnet.solar_max = solar_max;
+
+			/* Insesrt the new model */
+			if (nnetMap.find(bank_vol) == nnetMap.end()) {
+				Level2Map l2map;
+				nnetMap.insert(std::pair<double, Level2Map >(bank_vol, l2map));
+			}
+			nnetMap.find(bank_vol)->second.insert(std::pair<double, nnetmodel>(solar_min, nnet));
 		}
 	}
 
-	return 0;
 }
