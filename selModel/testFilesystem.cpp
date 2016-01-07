d67664fad0e3c5a80a1b81fd820d6e878c167494
Yukan Zhang <yzhan158@syr.edu>
2016-01-07 04:31:07 -0500

    updates testFilesystem.cpp
      Separate the test code by adding ifdef

--- selModel/testFilesystem.cpp
+++ selModel/testFilesystem.cpp
@@ -28,31 +28,6 @@ using std::vector;
  *   4. http://stackoverflow.com/questions/1257721/can-i-use-a-mask-to-iterate-files-in-a-directory-with-boost
  */
 
-int main(int argc, char *argv[]) {
-
-	map<double, Level2Map> nnetMap;
-	buildNnetMap(nnetMap);
-
-	cout << "###############################" << endl;
-	cout << "## Start recall process" << endl;
-	cout << "###############################" << endl;
-
-	std::vector<pair<double, double> > testcase;
-	testcase.push_back(make_pair(1400.0, 0.5));
-	testcase.push_back(make_pair(1400.0, 1.5));
-	testcase.push_back(make_pair(1400.0, 2.5));
-	for (std::vector<pair<double, double> >::iterator iter(testcase.begin()); iter != testcase.end(); ++iter) {
-		const nnetmodel &nmodel = recallNnetMap(nnetMap, iter->first, iter->second);
-		cout << "Bank charge: " << nmodel.bank_vol << endl;
-		cout << "Solar min: " << nmodel.solar_min << endl;
-		cout << "Solar max: " << nmodel.solar_max << endl;
-		cout << "Energy offset: " << nmodel.energy_offset << endl;
-		cout << "model IW: " << nmodel.IW << endl;
-		cout << "model LW: " << nmodel.LW << endl;
-	}
-	return 0;
-}
-
 const nnetmodel &recallNnetMap(const map<double, Level2Map> &nnetMap, double bankCharge, double solarPower) {
 
 	// Find the 1st levle key
@@ -139,3 +114,125 @@ void buildNnetMap(map<double, Level2Map> &nnetMap) {
 	}
 
 }
+
+#ifdef TEST_FILESYSTEM_MAIN
+int main(int argc, char *argv[]) {
+
+	map<double, Level2Map> nnetMap;
+	buildNnetMap(nnetMap);
+
+	cout << "###############################" << endl;
+	cout << "## Start recall process" << endl;
+	cout << "###############################" << endl;
+
+	std::vector<pair<double, double> > testcase;
+	testcase.push_back(make_pair(1400.0, 0.5));
+	testcase.push_back(make_pair(1400.0, 1.5));
+	testcase.push_back(make_pair(1400.0, 2.5));
+	testcase.push_back(make_pair(1450.0, 0.5));
+	testcase.push_back(make_pair(1435.0, 1.5));
+	testcase.push_back(make_pair(1476.0, 2.5));
+
+	testcase.push_back(make_pair(1300.0, 0.5));
+	testcase.push_back(make_pair(1300.0, 1.5));
+	testcase.push_back(make_pair(1300.0, 2.5));
+	testcase.push_back(make_pair(1375.0, 0.5));
+	testcase.push_back(make_pair(1303.0, 1.5));
+    testcase.push_back(make_pair(1350.0, 2.5));
+
+    testcase.push_back(make_pair(1200.0, 0.5));
+    testcase.push_back(make_pair(1200.0, 1.5));
+    testcase.push_back(make_pair(1200.0, 2.5));
+    testcase.push_back(make_pair(1295.0, 0.5));
+    testcase.push_back(make_pair(1253.0, 1.5));
+    testcase.push_back(make_pair(1290.0, 2.5));
+
+    testcase.push_back(make_pair(1100.0, 0.5));
+    testcase.push_back(make_pair(1100.0, 1.5));
+    testcase.push_back(make_pair(1100.0, 2.5));
+    testcase.push_back(make_pair(1175.0, 0.5));
+    testcase.push_back(make_pair(1103.0, 1.5));
+    testcase.push_back(make_pair(1150.0, 2.5));
+
+    testcase.push_back(make_pair(1000.0, 0.5));
+    testcase.push_back(make_pair(1000.0, 1.5));
+    testcase.push_back(make_pair(1000.0, 2.5));
+    testcase.push_back(make_pair(1075.0, 0.5));
+    testcase.push_back(make_pair(1003.0, 1.5));
+    testcase.push_back(make_pair(1050.0, 2.5));
+
+    testcase.push_back(make_pair(900.0, 0.5));
+    testcase.push_back(make_pair(900.0, 1.5));
+    testcase.push_back(make_pair(900.0, 2.5));
+    testcase.push_back(make_pair(975.0, 0.5));
+    testcase.push_back(make_pair(903.0, 1.5));
+    testcase.push_back(make_pair(950.0, 2.5));
+
+    //testcase.push_back(make_pair(800.0, 0.5));
+    //testcase.push_back(make_pair(800.0, 1.5));
+    testcase.push_back(make_pair(800.0, 2.5));
+   // testcase.push_back(make_pair(875.0, 0.5));
+   // testcase.push_back(make_pair(803.0, 1.5));
+    testcase.push_back(make_pair(850.0, 2.5));
+
+    testcase.push_back(make_pair(700.0, 0.5));
+    testcase.push_back(make_pair(700.0, 1.5));
+    testcase.push_back(make_pair(700.0, 2.5));
+    testcase.push_back(make_pair(775.0, 0.5));
+    testcase.push_back(make_pair(703.0, 1.5));
+    testcase.push_back(make_pair(750.0, 2.5));
+
+    testcase.push_back(make_pair(600.0, 0.5));
+    testcase.push_back(make_pair(600.0, 1.5));
+    testcase.push_back(make_pair(600.0, 2.5));
+    testcase.push_back(make_pair(675.0, 0.5));
+    testcase.push_back(make_pair(603.0, 1.5));
+    testcase.push_back(make_pair(650.0, 2.5));
+
+    testcase.push_back(make_pair(500.0, 0.5));
+    testcase.push_back(make_pair(500.0, 1.5));
+    testcase.push_back(make_pair(500.0, 2.5));
+    testcase.push_back(make_pair(575.0, 0.5));
+    testcase.push_back(make_pair(503.0, 1.5));
+    testcase.push_back(make_pair(550.0, 2.5));
+
+    //testcase.push_back(make_pair(400.0, 0.5));
+    //testcase.push_back(make_pair(400.0, 1.5));
+    testcase.push_back(make_pair(400.0, 2.5));
+    //testcase.push_back(make_pair(475.0, 0.5));
+    //testcase.push_back(make_pair(403.0, 1.5));
+    testcase.push_back(make_pair(450.0, 2.5));
+
+    testcase.push_back(make_pair(300.0, 0.5));
+    testcase.push_back(make_pair(300.0, 1.5));
+    testcase.push_back(make_pair(300.0, 2.5));
+    testcase.push_back(make_pair(375.0, 0.5));
+    testcase.push_back(make_pair(303.0, 1.5));
+    testcase.push_back(make_pair(350.0, 2.5));
+
+    testcase.push_back(make_pair(200.0, 0.5));
+    testcase.push_back(make_pair(200.0, 1.5));
+    testcase.push_back(make_pair(200.0, 2.5));
+    testcase.push_back(make_pair(275.0, 0.5));
+    testcase.push_back(make_pair(203.0, 1.5));
+    testcase.push_back(make_pair(250.0, 2.5));
+
+    testcase.push_back(make_pair(100.0, 0.5));
+    testcase.push_back(make_pair(100.0, 1.5));
+    testcase.push_back(make_pair(100.0, 2.5));
+    testcase.push_back(make_pair(175.0, 0.5));
+    testcase.push_back(make_pair(103.0, 1.5));
+    testcase.push_back(make_pair(150.0, 2.5));
+
+	for (std::vector<pair<double, double> >::iterator iter(testcase.begin()); iter != testcase.end(); ++iter) {
+		const nnetmodel &nmodel = recallNnetMap(nnetMap, iter->first, iter->second);
+		cout << "Bank charge: " << nmodel.bank_vol << endl;
+		cout << "Solar min: " << nmodel.solar_min << endl;
+		cout << "Solar max: " << nmodel.solar_max << endl;
+		cout << "Energy offset: " << nmodel.energy_offset << endl;
+		cout << "model IW: " << nmodel.IW << endl;
+		cout << "model LW: " << nmodel.LW << endl;
+	}
+	return 0;
+}
+#endif
