4dd50e4c190ea1eca43d147b8e258c4220e303d9
Yukan Zhang <yzhan158@syr.edu>
2016-01-22 03:57:20 -0500

    add testFilesystem.hpp

---
+++ selModel/testFilesystem.hpp
@@ -0,0 +1,14 @@
+#ifndef _TEST_FILESYSMTE_HPP_
+#define _TEST_FILESYSMTE_HPP_
+
+#include <map>
+
+#include "nnet.hpp"
+
+typedef std::map<double, nnetmodel> Level2Map;
+
+void buildNnetMap(std::map<double, Level2Map> &nnetMap);
+
+const nnetmodel &recallNnetMap(const std::map<double, Level2Map> &nnetMap, double bankCharge, double solarPower);
+
+#endif
